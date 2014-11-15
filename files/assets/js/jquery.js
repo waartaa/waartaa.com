5b73a288581926195f73bdec4a6c220ab60d63e0
Ratnadeep Debnath <ratnadeep.debnath@ibibogroup.com>
2014-11-15 12:38:48 +0530

    Added missing jquery js file.

---
+++ files/assets/js/jquery.js
@@ -0,0 +1,12 @@
+--2014-11-15 12:38:22--  http://code.jquery.com/jquery-1.11.1.min.js
+Resolving code.jquery.com (code.jquery.com)... 94.31.29.53, 94.31.29.230
+Connecting to code.jquery.com (code.jquery.com)|94.31.29.53|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 95786 (94K) [application/x-javascript]
+Saving to: ‘jquery-1.11.1.min.js’
+
+     0K .......... .......... .......... .......... .......... 53% 2.67M 0s
+    50K .......... .......... .......... .......... ...       100% 3.85M=0.03s
+
+2014-11-15 12:38:22 (3.12 MB/s) - ‘jquery-1.11.1.min.js’ saved [95786/95786]
+
